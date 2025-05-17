c4c96a550d7117e38a0c0e6ae044dc82d2b5e494
ThomyQP <thomasromerorobles@gmail.com>
2025-05-16 22:02:27 -0300

    Delete CustomLogicListener.js

--- CustomLogicListener.js
+++
@@ -1,9 +0,0 @@
-import LogicListener from "./generated/LogicListener.js";
-
-export class CustomLogicListener extends LogicListener {
-
-    enterStat(ctx) {
-        console.log(`Se detectó una: ${ctx.constructor.name}`);
-    }
-
-}
